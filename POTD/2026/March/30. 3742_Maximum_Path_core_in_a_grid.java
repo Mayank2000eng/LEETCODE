252a7dc2d3ba50ed4a537375a4f0a4fa9cafc9df
Mayank2000eng <mayankk2004k@gmail.com>
2026-04-30 18:40:33 +0530

    Changing the code

--- POTD/2026/March/30. 3742_Maximum_Path_core_in_a_grid.java
+++ POTD/2026/March/30. 3742_Maximum_Path_core_in_a_grid.java
@@ -24,6 +24,7 @@ class Solution {
             ans1=find(grid,i+1,j,k+c,K);
             ans2=find(grid,i,j+1,k+c,K);
         }
+
         if(ans1==-1&&ans2==-1) return dp[i][j][k]=-1;
         return dp[i][j][k]=grid[i][j]+Math.max(ans1,ans2);
     }
